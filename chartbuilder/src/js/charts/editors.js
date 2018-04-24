3b8b1990630df1ab7dec44adb981bdac475a6c16
Louise Yang <louise.m.yang@gmail.com>
2018-04-24 15:19:22 -0700

    Remove mobile settings options

--- chartbuilder/src/js/charts/editors.js
+++ chartbuilder/src/js/charts/editors.js
@@ -5,12 +5,10 @@
 // Editor components for chart types, as well as their mobile override editor interfaces
 module.exports = {
 	xy: {
-		Editor: require("../components/chart-xy/XYEditor.jsx"),
-		MobileOverrides: require("../components/chart-xy/XYMobile.jsx")
+		Editor: require("../components/chart-xy/XYEditor.jsx")
 	},
 
 	chartgrid: {
-		Editor: require("../components/chart-grid/ChartGridEditor.jsx"),
-		MobileOverrides: require("../components/chart-grid/ChartGridMobile.jsx")
+		Editor: require("../components/chart-grid/ChartGridEditor.jsx")
 	}
 };
